b4d88fd513bc3995389323bd80bf359aea61197f
Daniel Valdez <valdezdaniel1612@gmail.com>
2024-01-09 20:20:37 -0300

    segundaPreEntrega

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 // Importamos Express
 import express from "express";
-import ProductosRouter from "./router/productos.routes.js"
-import CarritoRouter from "./router/carritos.routes.js";
+import { engine } from "express-handlebars";
+import * as path from "path";
+import __dirname from "./utils.js";
+import connectDB from "./config/index.js";
+import viewsRouter from "./router/views.routes.js";
+import appRouter from "./router/index.routes.js";
 // Instanciamos la app de Express con la que manejaremos el servidor
 const app = express();
 // puerto donde correrá nuestro servidor
 const puerto = 8080;
+// Conexion a Atlas
+connectDB();
 // Le decimos a nuestra app que utilizaremos el formato JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/productos", ProductosRouter)
-app.use("/api/carritos", CarritoRouter)
-app.listen(puerto, () => {
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "views"));
+app.use("/", express.static(__dirname + "/public"));
+
+app.use("/", viewsRouter);
+app.use("/", appRouter);
+
+const server = app.listen(puerto, () => {
   console.log(`Servidor funcionando en puerto: ${puerto} `);
 });
-
